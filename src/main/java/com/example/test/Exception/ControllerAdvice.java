83a7db46c4a8cc8e6378be1f72f412eea64c0eee
bryan <917243940@qq.com>
2019-08-19 10:54:10 +0800

    日志输出异常

--- src/main/java/com/example/test/Exception/ControllerAdvice.java
+++ src/main/java/com/example/test/Exception/ControllerAdvice.java
@@ -1,14 +1,17 @@
 package com.example.test.Exception;
 
 import com.example.test.util.CommonResult;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class ControllerAdvice {
 
     @ExceptionHandler(Exception.class)
-    public CommonResult handleException() {
+    public CommonResult handleException(Exception e) {
+        log.error(e.getMessage());
         return CommonResult.errorResult(-1, "系统繁忙，请稍候再试");
     }
 
